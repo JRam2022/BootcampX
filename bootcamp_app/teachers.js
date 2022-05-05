e62c93c6376701ccf5c269dd4d25ec2ad1387cf0
JRam2022 <jasonram778@gmail.com>
2022-05-04 20:50:42 -0700

    Add queries for students and teachers

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,16 +10,16 @@ const pool = new Pool ({
 const argv = process.argv.slice(2)
 console.log(argv)
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE '%${argv[0]}%'
-ORDER BY student_id
-LIMIT ${argv[1]};
+ORDER BY teacher;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 });
-
